ac2dcb5dba8b16f28e9500aaeee894aa905373be
byhyakimaro <moderrevolution090217@gmail.com>
2023-02-14 01:32:45 -0300

    save

--- pages/categories/[category].tsx
+++ pages/categories/[category].tsx
@@ -14,6 +14,7 @@ export default function Home({ category }: any) {
         {category.map((categoryItem : any, index: any) => {
           return (
             <>
+              <img src={  categoryItem.Image }></img>
               <div>{ categoryItem.Title }</div>
             </>
           )
