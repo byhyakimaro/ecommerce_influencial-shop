9063a139c4daca292a5ccd150c4084c09af36c94
byhyakimaro <moderrevolution090217@gmail.com>
2023-02-14 01:56:14 -0300

    save

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,6 +1,15 @@
 import Head from 'next/head'
+import { useForm } from 'react-hook-form'
 
 export default function Home() {
+  const { register, handleSubmit } = useForm()
+
+  async function handleRegister(data: any) {
+    
+    console.log(data)
+
+  }
+
   return (
     <>
       <Head>
@@ -9,7 +18,49 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Dashboard</div>
+      <form onSubmit={handleSubmit(handleRegister)}>
+        <div>Title</div>
+        <input
+        {...register('Title')} 
+        ></input>
+
+        <div>Image</div>
+        <input
+        {...register('Image')} 
+        ></input>
+
+        <div>productStock</div>
+        <input
+        {...register('productStock')} 
+        ></input>
+
+        <div>Price</div>
+        <input 
+        {...register('Price')}
+        ></input>
+
+        <div>loginCreateItem</div>
+        <input
+        {...register('loginCreateItem')} 
+        ></input>
+
+        <div>category</div>
+        <input
+        {...register('category')} 
+        ></input>
+
+        <div>active</div>
+        <input
+        {...register('active')} 
+        ></input>
+        
+        <div>urlProvider</div>
+        <input
+        {...register('urlProvider')} 
+        ></input>
+
+        <button type="submit">Register Item</button>
+      </form>
     </>
   )
 }
