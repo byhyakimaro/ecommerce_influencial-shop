0180ab608b30e940169a9aecc65aa75b15598d38
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-04 09:49:47 -0300

    css

--- pages/precart/[product].tsx
+++ pages/precart/[product].tsx
@@ -20,6 +20,7 @@ export default function Home({ product }: any) {
         <div className={ styles.productInfo } >
           <img src={ product.Image }></img>
           <div>{ product.Title }</div>
+          <h2>R${ product.Price }</h2>
         </div>
         <div className={ styles.buttonsInfo }>
           <h3>Produto Adicionado ao Carrinho</h3>
