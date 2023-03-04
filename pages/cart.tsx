2d11b754942ab2a8edfa606507f13c21381fbe59
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-04 04:16:49 -0300

    save

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header></Header>
-      <div>Shopping</div>
+      <div>Cart</div>
     </>
   )
 }
