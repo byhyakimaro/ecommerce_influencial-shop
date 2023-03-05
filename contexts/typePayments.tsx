4e371d13edd380845cce0dba01cb6969a84cfc54
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-05 18:38:34 -0300

    css

--- contexts/typePayments.tsx
+++ contexts/typePayments.tsx
@@ -1,5 +1,7 @@
 import styles from '@/styles/Home.module.css'
-import IconPix from '@/public/icon_pix.svg'
+import PixSVG from '@/public/icon_pix.svg'
+import CreditCardSVG from '@/public/credit-card.svg'
+import BarCodeSVG from '@/public/bar-code.svg'
 
 export default function TypePayments() {
   return (
@@ -7,10 +9,9 @@ export default function TypePayments() {
       <div className={ styles.containerTypePayments }>
         <h2>Formas de Pagamento</h2>
         <div className={ styles.typesPayments }>
-          <button>Cartao de Credito</button>
-          <IconPix></IconPix>
-          <button>PIX</button>
-          <button>Boleto</button>
+          <button><CreditCardSVG width={24} height={24}></CreditCardSVG>Cartao de Credito</button>
+          <button><PixSVG width={24} height={24}></PixSVG>PIX</button>
+          <button><BarCodeSVG width={24} height={24}></BarCodeSVG>Boleto</button>
         </div>
       </div>
     </>
