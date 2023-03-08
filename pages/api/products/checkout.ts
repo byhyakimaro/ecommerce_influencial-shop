d47ac1d78ba8087c42c33850cf4492d6992abf76
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-08 11:10:12 -0300

    save

---
+++ pages/api/products/checkout.ts
@@ -0,0 +1,12 @@
+import { getCollection } from '@/modules/connectDb'
+import { ObjectId } from 'mongodb'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { token } = req.body
+
+  res.status(200).json({})
+}
