48fb0bd1fe0b14d8051a67842aaa11bc9adc55c7
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-09 02:05:36 -0300

    save

--- pages/api/products/listpurchased.ts
+++ pages/api/products/listpurchased.ts
@@ -11,7 +11,25 @@ export default async function handler(
   const collectionUsers = await getCollection('users')
   const dataCollection = await collectionUsers.findOne({ _id: new ObjectId(token)})
 
-  console.log(dataCollection.itemsPurchased)
+  dataCollection.itemsPurchased.map(async (itemPurchased:any) => {
 
-  res.status(200).json({})
+    const products = await Promise.all(itemPurchased.products.map(async (productId:any) =>{
+      const product = await fetch(`http://${req?.headers.host}/api/product/${productId}`)
+
+      if (product.status === 200) {
+        return await product.json()
+      }
+    }))
+
+    return {
+      id: itemPurchased.id,
+      status: itemPurchased.status,
+      data: itemPurchased.data,
+      url: itemPurchased.url,
+      methodPayment: itemPurchased.methodPayment,
+      products: products
+    }
+  })
+
+  res.status(200).json(dataCollection.itemsPurchased)
 }
