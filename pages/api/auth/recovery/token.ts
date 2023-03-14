1d8a8e85744bac5ac6db7f5c03bb1ce21ccc2a41
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-14 03:08:28 -0300

    save

--- pages/api/auth/recovery/token.ts
+++ pages/api/auth/recovery/token.ts
@@ -48,6 +48,7 @@ export default async function handler(
         name: dataCollection.name,
         login: dataCollection.login,
         currency: dataCollection.currency,
+        language: dataCollection.languages,
         email: dataCollection.email,
         savedAddresses: dataCollection.savedAddresses,
         telephone: dataCollection.telephone,
