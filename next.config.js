4ebc772a8dd25b37f0fb1d9ccd612a6d71ab34cd
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-15 05:13:27 -0300

    save

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
-module.exports = {
+  i18n: {
+    locales: ['en','pt'],
+    defaultLocale: 'en'
+  },
   images: {
     domains: ['i.imgur.com'],
   },
@@ -18,3 +18,5 @@ module.exports = {
     return config;
   }
 }
+
+module.exports = nextConfig
