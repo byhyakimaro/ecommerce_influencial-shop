f2b77309f5c1826ed2b6893a3983ad6dbf1c0537
byhyakimaro <moderrevolution090217@gmail.com>
2023-03-20 12:45:10 -0300

    save

--- pages/provider/[product].tsx
+++ pages/provider/[product].tsx
@@ -58,6 +58,7 @@ export default function Home({ product, i18n, User }: any) {
             <div className={styles.moreSell}><label>Mais Vendido</label><a>1º em Celulares e Smartphones</a> </div>
             <div className={ styles.Price } > { (product.Price).toLocaleString(language, {style: 'currency', currency: currency}) } </div>
             <div>{i18n.offPix}</div>
+            <h2>melhores fornecedores selecionados do brasil</h2>
             <button onClick={() => setShowComponent(true)}>{i18n.payments}</button>
             <div className={ styles.buyBottom }>
               <a href={ `../precart/${ product.Code }` }>
