9e3381af23b48fdff44b1ae5dd5508412143f9b9
byHyakimaro <moderrevolution090217@gmail.com>
2023-05-25 01:26:58 -0300

    save

--- pages/provider/[product].tsx
+++ pages/provider/[product].tsx
@@ -221,7 +221,7 @@ Home.getInitialProps = async (ctx: any) => {
       body: JSON.stringify({ search: product.Model })
     })
   
-    const similarProducts = (await fetchA.json()).filter((item:any) => item.Code === product.Code)
+    const similarProducts = (await fetchA.json()).filter((item:any) => item['_id'] !== product.Code)
 
     return {
       i18n: locales,
