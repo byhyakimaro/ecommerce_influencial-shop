a4eefef952029af7c0f1a547a565d3320d3552f7
byHyakimaro <moderrevolution090217@gmail.com>
2023-05-25 04:44:02 -0300

    save

---
+++ pages/api/admin/queuePurchased.ts
@@ -0,0 +1,16 @@
+import { ObjectId } from 'mongodb'
+import { getCollection } from '@/modules/connectDb'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const collection = await (await getCollection('users')).find().toArray()
+
+  const itemsPurchased = collection.map((item: any)=>{
+    return item.itemsPurchased
+  })
+
+  res.status(200).json(itemsPurchased)
+}
